df14e7dab16a3e17cf4787d81d82ed15de8b4e9c
yashmane0502-stack <yashmane0502@gmail.com>
2026-02-16 12:39:14 +0530

    Update fuel_eficiency_app.py

--- fuel_eficiency_app.py
+++ fuel_eficiency_app.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-
-
 import streamlit as st
 import pandas as pd
 import joblib
@@ -18,24 +15,35 @@ weight = st.number_input("weight", 0.0, 10000.0)
 acceleration = st.number_input("acceleration", 0.0, 50.0)
 model_year = st.number_input("model year", 1900, 2100)
 origin = st.number_input("origin", 1, 3)
-car_name = st.selectbox("car name", encoder["car name"].classes_)
 
-df = pd.DataFrame({
-    "mpg": [mpg],
-    "cylinders": [cylinders],
-    "displacement": [displacement],
-    "horsepower": [horsepower],
-    "weight": [weight],
-    "acceleration": [acceleration],
-    "model year": [model_year],
-    "origin": [origin],
-    "car name": [car_name]
-})
+# Handle encoder type safely
+if isinstance(encoder, dict):
+    car_name = st.selectbox("car name", encoder["car name"].classes_)
+else:
+    car_name = st.text_input("car name")
 
 if st.button("Predict"):
-    for col in encoder:
-        df[col] = encoder[col].transform(df[col])
 
+    df = pd.DataFrame({
+        "mpg": [mpg],
+        "cylinders": [cylinders],
+        "displacement": [displacement],
+        "horsepower": [horsepower],
+        "weight": [weight],
+        "acceleration": [acceleration],
+        "model year": [model_year],
+        "origin": [origin],
+        "car name": [car_name]
+    })
+
+    # Encoding
+    if isinstance(encoder, dict):
+        for col in encoder:
+            df[col] = encoder[col].transform(df[col])
+    else:
+        df["car name"] = encoder.transform(df["car name"])
+
+    # Match training column order
     df = df[model.feature_names_in_]
 
     prediction = model.predict(df)
